cd63ea01e531680ceff587c3c4463ab81e1c172f
NickFromSiths <113193039+NickFromSiths@users.noreply.github.com>
2024-11-13 21:33:49 -0500

    .

--- app/JS/main.js
+++ app/JS/main.js
@@ -7,11 +7,18 @@ const DOMSelectors = {
   button: document.querySelector(".sub"),
   description: document.querySelector("#description"),
   container: document.querySelector(".container"),
+  reset: document.querySelector("#reset"),
+  option: document.querySelector("#sale"),
   option1: document.querySelector("#one"),
-  theme1: document.querySelector(".theme"),
+  option2: document.querySelector("#two"),
+  option3: document.querySelector("#three"),
+  option4: document.querySelector("#four"),
+  option5: document.querySelector("#five"),
+  themee: document.querySelector(".theme"),
 };
 
 function cards() {
+  document.querySelector(".container").innerHTML = "";
   techProducts.forEach((element) => {
     if (element["salePrice"]) {
       const specific = `<div class="card"> 
@@ -21,7 +28,7 @@ function cards() {
           <p class="Description">Sale Price: ${element["salePrice"]}</p> 
           <p class="Description">Brand: ${element["brand"]}</p> 
           <p class="Description">Features: ${element["features"]}</p> 
-          <img src="${element[""]}" alt="${element[""]}" class="Image"></img>
+          <img src="${element["img"]}" alt="${element["name"]}" class="Image"></img>
       </div>`;
 
       DOMSelectors.container.insertAdjacentHTML("beforeend", specific);
@@ -31,29 +38,119 @@ function cards() {
           <p class="Description">Price: ${element["price"]}</p> 
           <p class="Description">Brand: ${element["brand"]}</p> 
           <p class="Description">Features: ${element["features"]}</p> 
-          <img src="${element[""]}" alt="${element[""]}" class="Image"></img>
+          <img src="${element["img"]}" alt="${element["name"]}" class="Image"></img>
       </div>`;
 
       DOMSelectors.container.insertAdjacentHTML("beforeend", specific);
     }
   });
-  console.log("1");
 }
+DOMSelectors.reset.addEventListener("click", function (event){
+  event.preventDefault();
+  cards();
+})
+
 
 cards();
 
-function showByGenre(x) {
+
+
+function saleprice(x) {
   document.querySelector(".container").innerHTML = "";
-  const filtered = techProducts.filter((element) => element.salePrice);
-  DOMSelectors.container.insertAdjacentHTML("beforeend", filtered);
+  const filtered = techProducts.filter((element) => element[x]);
+  filtered.forEach((element) => {
+      const specific = `<div class="card"> 
+
+          <h2 class="Title">${element["name"]}</h2> 
+          <p class="Description">Price: ${element["price"]}</p> 
+          <p class="Description">Sale Price: ${element["salePrice"]}</p> 
+          <p class="Description">Brand: ${element["brand"]}</p> 
+          <p class="Description">Features: ${element["features"]}</p> 
+          <img src="${element["img"]}" alt="${element["name"]}" class="Image"></img>
+      </div>`;
+
+      DOMSelectors.container.insertAdjacentHTML("beforeend", specific);
+    }
+  );
 }
-DOMSelectors.option1.addEventListener("click", function (event) {
+
+//filter items only on sale 
+DOMSelectors.option.addEventListener("click", function (event) {
   event.preventDefault();
-  showByGenre();
+  saleprice("salePrice");
 });
 
-DOMSelectors.theme1.addEventListener("click", function (event) {
+function sortbrand(x){
+  document.querySelector(".container").innerHTML = "";
+  const filtered = techProducts.filter((element) => element.brand == x);
+  filtered.forEach((element) => {
+    if (element["salePrice"]) {
+      const specific = `<div class="card"> 
+
+          <h2 class="Title">${element["name"]}</h2> 
+          <p class="Description">Price: ${element["price"]}</p> 
+          <p class="Description">Sale Price: ${element["salePrice"]}</p> 
+          <p class="Description">Brand: ${element["brand"]}</p> 
+          <p class="Description">Features: ${element["features"]}</p> 
+          <img src="${element["img"]}" alt="${element["name"]}" class="Image"></img>
+      </div>`;
+
+      DOMSelectors.container.insertAdjacentHTML("beforeend", specific);
+    } else {
+      const specific = `<div class="card"> 
+          <h2 class="Title">${element["name"]}</h2> 
+          <p class="Description">Price: ${element["price"]}</p> 
+          <p class="Description">Brand: ${element["brand"]}</p> 
+          <p class="Description">Features: ${element["features"]}</p> 
+          <img src="${element["img"]}" alt="${element["name"]}" class="Image"></img>
+      </div>`;
+
+      DOMSelectors.container.insertAdjacentHTML("beforeend", specific);
+    }
+    }
+  );
+}
+DOMSelectors.option1.addEventListener("click", function (event){
   event.preventDefault();
+  sortbrand("Brand A");
 });
+
+DOMSelectors.option2.addEventListener("click", function (event){
+  event.preventDefault();
+  sortbrand("Brand B");
+});
+DOMSelectors.option3.addEventListener("click", function (event){
+  event.preventDefault();
+  sortbrand("Brand C");
+});
+DOMSelectors.option4.addEventListener("click", function (event){
+  event.preventDefault();
+  sortbrand("Brand D");
+});
+DOMSelectors.option5.addEventListener("click", function (event){
+  event.preventDefault();
+  sortbrand("Brand E");
+});
+
+
+
+
+
+//theme change 
+DOMSelectors.themee.addEventListener("click", function (event) {
+  event.preventDefault();
+  if(document.body.classList.contains("light")){
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  }else{
+    document.body.classList.remove("dark");
+    document.body.classList.add("light")
+  }
+});
+
+
+
+
+
 // DOMSelectors.form.addEventListener()
 // for checkbox form for sorting cards
